88369337b970e635b6f6ab62502dff8f009a18f6
shanndev28 <fajarkhairul723@gmail.com>
2023-09-03 16:26:37 +0700

    ubah command

--- library/router/whatsapp/cmd/tools/topdf.js
+++ library/router/whatsapp/cmd/tools/topdf.js
@@ -10,7 +10,7 @@ module.exports = {
             let file = (await msg.download('buffer') || (msg.quoted && (await msg.quoted.download('buffer'))))
 
             await fs.writeFileSync('library/upload/' + path + '.jpg', file)
-            let filedata = await lolhuman('convert2pdf?filename=' + path + '.jpg' + '&file=' + process.env.BASE_URL + 'upload/' + path + '.jpg')
+            let filedata = await lolhuman('convert2pdf?filename=' + path + '.jpg' + '&filename=' + path + '.jpg&file=' + process.env.BASE_URL + 'upload/' + path + '.jpg')
 
             await fs.unlinkSync('library/upload/' + path + '.jpg')
 
